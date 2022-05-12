af46808eff6e65b9da3a39221afe5bc0479fba34
mfujishi <mfujishi@student.42tokyo.jp>
2022-05-12 23:38:13 +0900

    ctrl+cの修正。不完全なので、可能だったらwhileの中に入れれるようにします。

--- src/minishell_signal.c
+++ src/minishell_signal.c
@@ -2,7 +2,11 @@
 
 void	signal_ctrl_c()
 {
-	write(1, "\n", 1);
+	// write(1, "\n", 1);
+	static int i;
+	if (i == 0)
+		write(1, "\n", 1);
+	write(1, "minishell > ", 12);
 }
 
 void	perr_exit(char *err_msg)
@@ -15,18 +19,16 @@ int	minishell_signal(char *command)
 {
 	struct sigaction ctrl_c;
 	struct sigaction ctrl_bs;
-	
+
 	ft_memset(&ctrl_c, 0, sizeof(ctrl_c));
 	ctrl_c.sa_handler = signal_ctrl_c;
 	ctrl_c.sa_flags = SA_RESTART;
 	sigemptyset(&ctrl_c.sa_mask);
-
 	ft_memset(&ctrl_bs, 0, sizeof(ctrl_bs));
 	ctrl_bs.sa_handler = SIG_IGN;
 	ctrl_bs.sa_flags = 0;
 	if (sigemptyset(&ctrl_bs.sa_mask))
 		perr_exit("emptyset");
-
 	if (sigaction(SIGINT, &ctrl_c, NULL) < 0)
 		perr_exit("sigaction");
 	if (sigaction(SIGQUIT, &ctrl_bs, NULL) < 0)
