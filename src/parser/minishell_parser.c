119e67e4e4eac93e122ea56379288520c058ed1b
mfujishi <mfujishi@student.42tokyo.jp>
2022-05-17 13:54:27 +0900

    parser

--- src/parser/minishell_parser.c
+++ src/parser/minishell_parser.c
@@ -1,7 +1,31 @@
 #include "minishell.h"
 
+static int	check_pipe_error(t_token *token)
+{
+	if (token->type == PIPE)
+	{
+		if (token->next->type == PIPE || token->next->type == TAIL)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
 
-int parser(t_token *token, t_envlist *env)
+static int	check_redirect_error(t_token *token)
+{
+	if (token->type == REDIRECT || token->type == R_STDIN)
+	{
+		if (token->next->type == REDIRECT || token->next->type == PIPE || \
+		token->next->type == TAIL || token->next->type == R_STDIN)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+
+int	parser(t_token *token, t_envlist *env)
 {
 	if (token->type == HEAD && token->next->type == PIPE)
 	{
@@ -10,18 +34,16 @@ int parser(t_token *token, t_envlist *env)
 	}
 	while (token->type != TAIL)
 	{
-		if (token->type == PIPE)
-			if (token->next->type == PIPE || token->next->type == TAIL)
-			{
-				error("Error \"syntax error around pipe\"", 1, env);
-				return (1);
-			}
-		if (token->type == REDIRECT || token->type == R_STDIN)
-			if (token->next->type == REDIRECT || token->next->type == PIPE || token->next->type == TAIL || token->next->type == R_STDIN)
-			{
-				error("Error \"syntax error around redirect\"", 1, env);
-				return (1);
-			}
+		if (check_pipe_error(token) == 1)
+		{
+			error("Error \"syntax error around pipe\"", 1, env);
+			return (1);
+		}
+		if (check_redirect_error(token) == 1)
+		{
+			error("Error \"syntax error around redirect\"", 1, env);
+			return (1);
+		}
 		token = token->next;
 	}
 	return (0);
