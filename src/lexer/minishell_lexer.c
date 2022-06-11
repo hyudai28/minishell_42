d2e625059967fd7d35f330363a5e025751038c0f
mfujishi <mfujishi@student.42tokyo.jp>
2022-06-11 22:40:47 +0900

    lexer norm

--- src/lexer/minishell_lexer.c
+++ src/lexer/minishell_lexer.c
@@ -1,4 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell_lexer.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfujishi <mfujishi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/11 22:39:48 by mfujishi          #+#    #+#             */
+/*   Updated: 2022/06/11 22:40:38 by mfujishi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
+
+static int	lexer_error(void *p, char *argv, t_token *head, t_envlist *env)
+{
+	if (p == NULL)
+	{
+		free(argv);
+		token_destructor(head);
+		error("minishell: Cannot allocate memory", 1, env);
+		return (1);
+	}
+	return (0);
+}
 
 int	lexer(char *argv, t_token *head, t_envlist *env)
 {
@@ -11,23 +35,13 @@ int	lexer(char *argv, t_token *head, t_envlist *env)
 	while (*str != '\0')
 	{
 		cur = new_token();
-		if (cur == NULL)
-		{
-			free(argv);
-			token_destructor(head);
-			error("minishell: Cannot allocate memory", 1, env);
+		if (lexer_error(cur, argv, head, env) == 1)
 			return (1);
-		}
 		add_token_last(head, cur);
 		get_token(cur, str);
 		cur->word = (char *)malloc(sizeof(char) * cur->word_len + 1);
-		if (cur->word == NULL)
-		{
-			free(argv);
-			token_destructor(head);
-			error("minishell: Cannot allocate memory", 1, env);
+		if (lexer_error(cur->word, argv, head, env) == 1)
 			return (1);
-		}
 		ft_strlcpy(cur->word, str, cur->word_len + 1);
 		str += cur->word_len;
 		while (ft_isspace(*str))
