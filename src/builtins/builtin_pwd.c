20e46b3133c018466824704f41b72b4cdeff4704
hyudai28 <hayasakayudai@gmail.com>
2022-06-20 22:10:13 +0900

    pwd error case

--- src/builtins/builtin_pwd.c
+++ src/builtins/builtin_pwd.c
@@ -3,14 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_pwd.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mfujishi <mfujishi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: hyudai <hyudai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 01:45:57 by mfujishi          #+#    #+#             */
-/*   Updated: 2022/06/13 01:45:58 by mfujishi         ###   ########.fr       */
+/*   Updated: 2022/06/20 22:09:55 by hyudai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	pwd_error(t_envlist *env)
+{
+	t_envlist	*pwd_node;
+
+	pwd_node = envlist_search("PWD", env);
+	if (!pwd_node)
+	{
+		ft_putstr_fd("minishell: pwd: ", 2);
+		error(strerror(errno), 1, env);
+		return (1);
+	}
+	ft_putendl(pwd_node->value);
+	return (0);
+}
 
 int	builtin_pwd(char **cmds, int argc, t_envlist *env)
 {
@@ -20,6 +35,10 @@ int	builtin_pwd(char **cmds, int argc, t_envlist *env)
 	(void)argc;
 	(void)env;
 	pwd = getcwd(NULL, 0);
+	if (!pwd)
+	{
+		return (pwd_error(env));
+	}
 	write(1, pwd, ft_strlen(pwd));
 	free(pwd);
 	write(1, "\n", 1);
