5d09adeaa4ee3a0640729c7b567d2c1726773f80
mfujishi <mfujishi@student.42tokyo.jp>
2022-06-20 15:00:09 +0900

    ls|でもambiguousになるのを修正

--- src/expansion/minishell_expansion.c
+++ src/expansion/minishell_expansion.c
@@ -6,7 +6,7 @@
 /*   By: mfujishi <mfujishi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 01:38:44 by mfujishi          #+#    #+#             */
-/*   Updated: 2022/06/20 14:58:17 by mfujishi         ###   ########.fr       */
+/*   Updated: 2022/06/20 14:59:35 by mfujishi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,8 @@ static int	check_doller(t_token *token)
 
 static int	expansion_error(t_token *token)
 {
-	if (token_check_separate(token->prev->type) == 1)
+	if (token_check_separate(token->prev->type) == 1 &&
+		token->prev->type != PIPE)
 	{
 		ft_putstr_fd("minishell: ", 2);
 		ft_putstr_fd(token->word, 2);
